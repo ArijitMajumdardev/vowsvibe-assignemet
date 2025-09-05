7588727b2eceddbec806cb09e7e9ec0cc10cef7d
arijit <arijitstoic@gmail.com>
2025-09-05 22:58:42 +0530

    The first commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import ProductPage from "./components/ProductPage"
+
+
+function App() {
+
+
+  return (
+  
+      <ProductPage/>
+ 
+  )
+}
+
+export default App
---
+++ src/components/Header.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+      <div className='bg-[#faf9f6] h-[128px] w-full '>
+          <div className='bg-[#ca8787] h-[36px] md:h-[43px] w-full text-center text-white'>
+              50% off on all items Only Limited Time Deal Offer ending in 03:34:15
+          </div>
+    </div>
+  )
+}
+
+export default Header
